64bc8c0bf12b05489380a2f58115e0486bb80dcd
Manisha Dhyani <dhyanimanishals95@gmail.com>
2025-08-08 04:51:53 +0530

    initial commit - basic structure of SB

---
+++ src/test/java/com/springboot/ranga/learn_webservices/LearnWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ranga.learn_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
