272b43ecbc5c65b99b15989587bb752be8d31f4e
Manisha Dhyani <dhyanimanishals95@gmail.com>
2025-12-19 00:27:00 +0530

    Implemented Actuator and static filtering.

--- src/main/java/com/springboot/ranga/learn_webservices/socialmediaapp/model/User.java
+++ src/main/java/com/springboot/ranga/learn_webservices/socialmediaapp/model/User.java
@@ -1,5 +1,7 @@
 package com.springboot.ranga.learn_webservices.socialmediaapp.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -7,16 +9,22 @@ import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+//@JsonIgnoreProperties({"Name", "id"}) // can have single value or array of fields to ignore.
+// Static filtering at class level. This will ignore the specified fields during serialization/deserialization.
 public class User {
 
+    @JsonIgnore
+    // static filtering. This field will be ignored during serialization/deserialization .i.e it won't appear in JSON response
     private int id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
     @NotBlank(message = "Name cannot be BLANK, Please provide a Name.")
+    @JsonProperty("Name") // custom name for JSON property.
     private String name;
 
     @Past(message = "Date of Birth should be in the past, Please provide a valid Date of Birth.")
     @NotNull(message = "Date of Birth cannot be NULL, Please provide Date of Birth.")
+    @JsonProperty("Date_of_Birth")
     private LocalDate dob;
 
     public User(int id, String name, LocalDate dob) {
